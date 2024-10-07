d4092264dc42db320e8ca18552f72ff6b4f6e9c0
RTalaro <146395601+RTalaro@users.noreply.github.com>
2024-10-06 17:33:23 -0700

    refactor: basic functionality in title screen
    play, options, quit buttons working

---
+++ Bountiful X/farm.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print('farm ready')
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ Bountiful X/title.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var play_button = $MarginContainer/VBoxContainer2/VBoxContainer/Play
+@onready var options_button = $MarginContainer/VBoxContainer2/VBoxContainer/Options
+@onready var quit_button = $MarginContainer/VBoxContainer2/VBoxContainer/Quit
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# connect signals
+	play_button.button_down.connect(on_play_button_pressed)
+	options_button.button_down.connect(on_options_button_pressed)
+	quit_button.button_down.connect(on_quit_button_pressed)
+	
+	print('title ready')
+
+
+# define signals
+func on_play_button_pressed():
+	print('play')
+	get_tree().change_scene_to_file('res://farm.tscn')
+
+func on_options_button_pressed():
+	print('open options')
+	get_tree().change_scene_to_file('res://options.tscn')
+
+func on_quit_button_pressed():
+	print('quit')
+	get_tree().quit()
