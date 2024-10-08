a2b413a7181395c3aa4484a53549f1217a07b885
Stan-21 <shung951@gmail.com>
2024-10-07 20:54:26 -0700

    feat(crops): added system for crops
    Implemented placeholder tilemap for crops
    
    Can now "till" "water" and "plant" crops in dirt

--- Bountiful X/farm.gd
+++ Bountiful X/farm.gd
@@ -1,5 +1,8 @@
 extends Control
 
+var inventory_num = 0
+# 1 till soil, 2 water plants, 3 wheat, 4 corn, 5 parsnip (?), 6 carrots
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +12,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if int(event.as_text()) < 7:
+			inventory_num = 2 * int(event.as_text()) - 2
+	
+	if event is InputEventMouseButton and event.pressed:
+		$Dirt.update_tile(event.position)
+	
